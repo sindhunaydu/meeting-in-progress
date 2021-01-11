9d5fe939e03d08dd2c78094fd33896f6c29f5889
Sindhu Murthy <sindhu.naydu@gmail.com>
2021-01-10 18:05:16 -0800

    Update and rename switchController.py to buttonController.py

---
+++ buttonController.py
@@ -0,0 +1,38 @@
+from time import sleep
+from gpiozero import Button
+import ledBoardController
+
+mic_button = Button(11)
+audio_button = Button(26)
+video_button = Button(13)
+dnd_button = Button(6)
+off_button = Button(4)
+
+def led_state_1():
+    ledBoardController.led_off()
+    ledBoardController.led_mic_on()
+    
+def led_state_2():
+    ledBoardController.led_off()
+    ledBoardController.led_video_on()
+
+def led_state_3():
+    ledBoardController.led_off()
+    ledBoardController.led_audio_on()
+
+def led_state_4():
+    ledBoardController.led_off()
+    ledBoardController.led_dnd()
+
+def led_state_5():
+    ledBoardController.led_off()
+
+mic_button.when_activated = led_state_1
+audio_button.when_activated = led_state_2
+video_button.when_activated = led_state_3
+dnd_button.when_activated = led_state_4
+off_button.when_activated = led_state_5
+
+if __name__ == '__main__':
+    while True:
+        sleep(1)
